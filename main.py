2d68eeb602fe37bea6af2acad067d08fc2b387ce
Serhii Honchar <serhii.honchar.java@gmail.com>
2025-03-05 21:03:57 +0100

    Convert user input to speech.

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from gtts import gTTS
 import os
 
-with open('demo.txt', 'r', encoding='utf-8') as file:
-    file_content = file.read()
+import tkinter as tk
+from tkinter import messagebox
 
-language = 'en'
+root = tk.Tk()
+root.title("Speech Converter")
+canvas = tk.Canvas(root, width=270, height=200)
 
-audio_output = gTTS(text=file_content, lang=language, slow=False)
-audio_output.save('audio_output.mp3')
+canvas.pack()
 
-os.system('start audio_output.mp3')
+
+def textToSpeech():
+    text = entry.get()
+    if not text:
+        messagebox.showwarning("Warning", "No text selected for converting.")
+        return
+
+    language = 'en'
+    audio_output = gTTS(text=text, lang=language, slow=False)
+    audio_output.save('audio_output.mp3')
+    os.system('start audio_output.mp3')
+
+
+entry = tk.Entry(root)
+canvas.create_window(133, 80, width=250, window=entry)
+button = tk.Button(text="Start", width=15, command=textToSpeech)
+canvas.create_window(135, 110, window=button)
+
+exit_button = tk.Button(text="Exit", width=15, command=root.quit, highlightthickness=0)
+canvas.create_window(135, 140, window=exit_button)
+
+root.mainloop()
